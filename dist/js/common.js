fcef419069a72dfd80965c4762a441e3db1a34e2
Olha <ololoevaevaeva@gmail.com>
2020-02-27 15:42:24 +0200

    commit

---
+++ dist/js/common.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+    $('.card').each(function() {
+        let homeLength = $(this).find('.card-home-img').length;
+        if (homeLength <= 1) {
+            $(this).find(".card-home-img").removeClass("size");
+        } else {
+            $(this).find(".card-home-img").addClass("size");
+        }
+    });
+    $('.main-btns .btn').click(function() {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+
+    $('.player-item').click(function() {
+        $(this).toggleClass('active').siblings().removeClass('active');
+    });
+    $('.close').click(function() {
+        $(this).closest(".chat").hide();
+    });
+
+    $('.player-item, .player-item.height').hover(function() {
+        $(this).toggleClass('height');
+    });
+
+
+    let widthPlace = $(".player-item").width() - $(".player-col").width();
+    if($(".player-info_container").width() >= widthPlace){
+        $(".player-item").removeClass('show');
+    } else {
+        $(".player-item").addClass('show');
+    }
+
+    tippy('.card-price', {
+        content: 'Lorem ipsum dolor sit amet ',
+        placement: 'top',
+        theme: 'bg-medium',
+    });
+    tippy('.card-bookmark', {
+        content: 'Lorem ipsum dolor sit amet ',
+        placement: 'bottom-start',
+        theme: 'bg-strong',
+    });
+    tippy('.balls-item', {
+        content: 'Lorem ipsum dolor sit amet ',
+        placement: 'bottom',
+        theme: 'bg-light',
+    });
+    tippy('.card-img', {
+        content: 'Lorem ipsum dolor sit amet ',
+        placement: 'right',
+        theme: 'bg-light',
+    });
+});
